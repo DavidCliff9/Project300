803dd9b908421d719bdc827a190e0f955ee97c6d
DavidCliff9 <157513406+DavidCliff9@users.noreply.github.com>
2025-10-31 19:53:25 +0000

    Update create_medsuite_master.sql
    Added the tables with constraints and references

--- Pro300/sqlcode/create_medsuite_master.sql
+++ Pro300/sqlcode/create_medsuite_master.sql
@@ -51,7 +51,7 @@ END IF;
 CREATE SCHEMA Records;
 CREATE SCHEMA GP;
 CREATE SCHEMA Accounts;
-CREATE SCHEMA Users;
+CREATE SCHEMA Patients;
 
 -- User Privileges
 
@@ -77,18 +77,70 @@ BEGIN
 --Execute will allow the use of DDL commands
 
         EXECUTE '
-                CREATE TABLE IF NOT EXISTS users.users (
-                --Allows the incremention of id
-                id SERIAL PRIMARY KEY,
-                PatientName VARCHAR(255),
-                PlaceHolder VARCHAR(255)
+ 				CREATE TABLE IF NOT EXISTS patients.patients (
+                 -- Allows the incremention of id, Primary Key
+                User_Id SERIAL PRIMARY KEY,
+                First_Name VARCHAR(50),
+                Last_Name VARCHAR(50),
+                 -- Enforce Male, Female or Other
+                Gender VARCHAR(10)
+                        CHECK (Gender IN ('Male', 'Female', 'Other')),
+                Email VARCHAR(50) NOT NULL UNIQUE
+                 -- Eneforce Proper Email Entry (Any character before @, least one after before . and one after that)
+                        CHECK (Email LIKE('%@%_._%')
+                 -- Use Current Date if not date provided,
+                Signed_up_at DATE NOT NULL DEFAULT CURRENT_DATE
+	
+         		-- !! Table should NEVER be used in Queries !!
+                CREATE TABLE IF NOT EXISTS accounts.users_accounts (
+                 -- Allows the incremention of id, Primary Key
+                Account_Id SERIAL PRIMARY KEY,
+                 -- Foreign Key 1:1
+                User_Id INT NOT NULL UNIQUE,
+                Email VARCHAR(50) NOT NULL UNIQUE
+                 -- Eneforce Proper Email Entry (Any character before @, least one after before . and one after that)
+                        CHECK (Email LIKE('%@%_._%'))
+                Password_Hash VARCHAR(255) NOT NULL,
+                 -- Foreign Key Constraint
+                CONSTRAINT fk_user_id
+                        FOREIGN KEY (UserId)
+                        REFERENCES patients.patients (User_Id)
+                        ON DELETE CASCADE -- Delete Account info if patient is deleted WITHOUT records
         );
-                CREATE TABLE IF NOT EXISTS accounts.usersaccounts (
-                --Allows the incremention of id
-                id SERIAL PRIMARY KEY,
-                Name VARCHAR(255),
-                Description VARCHAR(255)
+
+
+                CREATE TABLE IF NOT EXISTS GP.gp (
+                 -- Allows the incremention of Primary Key
+                Gp_Id SERIAL PRIMARY KEY,
+                First_Name VARCHAR(50) NOT NULL,
+                Last_Name VARCHAR(50) NOT NULL,
+                role VARCHAR(50) NOT NULL
+                 -- Enforce Correct Role Entry
+                        CHECK (role in ('Surgeon','Doctor', 'Nurse', 'Physiotherapist')),
+                employed_at VARCHAR(100)
         );
+
+                CREATE TABLE IF NOT EXISTS records.records (
+                 -- Allows the incrementation of Primary Key
+                Record_Id SERIAL PRIMARY KEY,
+                 -- Foreign Keys to patient and GP table (Link Table)
+                User_Id INT NOT NULL,
+                Gp_Id INT NOT NULL,
+                 -- Body of the record
+                Record_Details TEXT NOT NULL,
+                Practice VARCHAR(100),
+                 -- If no value is entered, use the default timestamp
+                Created_At TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+                 -- Foreign Key Constraints
+                CONSTRAINT fk_user_record_id
+                        FOREIGN KEY (User_Id)
+                        REFERENCES patients.patients (User_Id)
+                        ON DELETE RESTRICT, -- Prevent Deletion of a patient WITH records
+                CONSTRAINT fk_gp_record_id
+                        FOREIGN KEY (Gp_Id)
+                        REFERENCES GP.gp (Gp_Id)
+                        ON DELETE RESTRICT -- Prevent Deletion of a GP WITH records
+        );	
         ';
 
         --Inform user if table already exists (Only works if run from select * from)
