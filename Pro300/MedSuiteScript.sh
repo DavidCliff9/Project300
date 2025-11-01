45aa7d4b47816939da81e7db57c22b3e4cebe931
DavidCliff9 <157513406+DavidCliff9@users.noreply.github.com>
2025-11-01 19:53:41 +0000

    Add files via upload
    Updated Medical Shell Script to Include working Creation Script

--- Pro300/MedSuiteScript.sh
+++ Pro300/MedSuiteScript.sh
@@ -1,63 +1,95 @@
 #!/bin/bash
-# This does things
+# This is a script that allows the automatic update of the Medical Suite Database, with deletion script for testing purposes
+
 
 # Hold the SQL Directory
-SQL_DIR="$HOME/sqlcode"
+SQL_DIR="/tmp/sqlcode"
 
 # Bootstrap script URL
-UPDATE_URL="https://raw.githubusercontent.com/DavidCliff9/Project300/main/Pro300/sqlcode/create_medsuite_master.sql"
+UPDATE_URL="https://raw.githubusercontent.com/DavidCliff9/Project300/refs/heads/main/Pro300/sqlcode/create_medsuite_master.sql"
 
 # Delete script URL
 
 #DELETE_URL=""
+
 # Name of the output file
 OUTPUT_FILENAME="bootstrap_medical_suite.sql"
+
+# Name of the databsae user
+DB_USER="postgres"
 
 # Check if the SQL Directory exists
 
 if [ -d "$SQL_DIR" ]; then
-	echo "SQLCode exists, skipping creation"
+	echo  "SQLCode Directory exists: Skipping Creation"
 else
-	echo "SQLCode does not exist, creating"
+	echo  "SQLCode does not exist, creating"
 	mkdir -p "$SQL_DIR"
-	echo -n "Created with delete script"
+	echo  "Created with delete script"
 
 fi
 
-# Check if the url is present
 
-if ["$UPDATE_URL" != "https://raw.githubusercontent.com/DavidCliff9/Project300/main/Pro300/sqlcode/create_medsuite_master.sql" ]; then
-	echo -n "URL for the Bootstrap Script has been moved! Contact David on Discord"
+
+# Check if the url is correct
+
+if [ "$UPDATE_URL" != "https://raw.githubusercontent.com/DavidCliff9/Project300/refs/heads/main/Pro300/sqlcode/create_medsuite_master.sql" ]; then
+	echo  "URL for the Bootstrap Script has been moved! Contact David on Discord"
+	echo "URL for debugging:"
+	echo -n "$UPDATE_URL"
+	# Terminate Program here
+	exit 1
 else
-	wget -O "$SQL_DIR/$OUT_FILENAME" "https://raw.githubusercontent.com/DavidCliff9/Project300/main/Pro300/sqlcode/create_medsuite_master.sql"
+	# Use wget to install the bootstrap script
+
+	wget -O "$SQL_DIR/$OUTPUT_FILENAME" "https://raw.githubusercontent.com/DavidCliff9/Project300/main/Pro300/sqlcode/create_medsuite_master.sql"
 fi
 
-#Exit Code for wget
+#Exit Code for wget to ensure file is correctly downloaded
 
-if [ $? -eq ]; then
-	echo -n "Latest File downloaded correctly"
+if [ $? -eq 0 ]; then
+	echo  "Latest File downloaded correctly"
 else
-	echo -n "File download failed. Check Connection"
+	echo  "File download failed. Check Connection"
 	exit 1
 fi
 
 # Prompt user for option
+# Single Qoutes for literal String
 
-echo -n "Delete or run the Database bootstrap?"
-echo -n "Delete (DEL)
-echo -n "Run Updated Bootstrap (CRE)
-echo -n "Exit (EXIT)
+echo  "Delete or run the Database bootstrap?"
+echo ""
+echo  '1. Run Updated Bootstrap Script (CRE)'
+echo  '2. Delete Database (DEL)'
+echo  '3. Exit (EXIT)'
 
 read choice
 
 # Loop if the option is incorrect
 
-while [ "$choice" != "CRE" OR "DEL" OR "EXIT" ]
+while ! ( [ "$choice" == "CRE" ] || [ "$choice" == "DEL" ] || [ "$choice" == "EXIT" ] )
 do
-	echo -n "Invalid Option. Select again"
+	echo -n  "Invalid Option. Select again: "
 	read choice
 done
 
-echo "program exitied"
+# Perform Action Based on Input
 
+if [ "$choice" == "CRE" ]; then
+	echo "Running Latest Database Bootstrap"
+	# Brackets are not needed as this is a command and not a test
+
+	if sudo -H -u "$DB_USER" psql -X -f "$SQL_DIR/$OUTPUT_FILENAME"; then
+		echo "Database Created/Updated Successfully"
+	else
+		echo "Database bootstrap failed. Check permissions or Postgres Logs"
+	fi
+
+elif [ "$choice" == "DEL" ]; then
+	echo "Deleting database"
+	# Add Script here
+
+elif [ "$choice" == "EXIT" ]; then
+	echo "Exiting program"
+fi
 
