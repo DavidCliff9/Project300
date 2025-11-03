d8c71d38720b9908d82fb59ee3e6db315665271d
DavidCliff9 <157513406+DavidCliff9@users.noreply.github.com>
2025-11-03 17:03:03 +0000

    Update medical_suite_delete.sql
    Fixed Script trying to connect to database before deletion

--- Pro300/sqlcode/medical_suite_delete.sql
+++ Pro300/sqlcode/medical_suite_delete.sql
@@ -1,9 +1,5 @@
 -- This SQL Script deletes the MedicalSuite database. Used for providing a clean install as apart of the bootstrap loader
 
--- The script will run as postgres super user. Connect to the existing MedicalSuite Database
-\c medicalsuite
-
 -- Drop Database command. If Exists stops the script from throwing an error if ran again. With Force terminates active connections
 
-DROP DATABASE [IF EXISTS] medicalsuite
-[WITH (FORCE)]
+DROP DATABASE IF EXISTS medicalsuite WITH (FORCE)
