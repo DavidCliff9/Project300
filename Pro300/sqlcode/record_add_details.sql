e4810b6f456e49c9dd7f13a78cb4bd92a912f238
DavidCliff9 <157513406+DavidCliff9@users.noreply.github.com>
2025-11-24 21:31:43 +0000

    Add files via upload
    Stored Procedure and Worker Function to insert a medical record

---
+++ Pro300/sqlcode/record_add_details.sql
@@ -0,0 +1,36 @@
+-- This Function will insert details once it passes checks from the stored procedure its called
+
+CREATE OR REPLACE FUNCTION add_record_details(EPatientEmail VARCHAR(50),
+EGPFirstName VARCHAR(50),
+EGPLastName VARCHAR(50),
+ERecordDetails TEXT,
+EPractice VARCHAR(50))
+
+-- Return int to master procedure
+
+RETURNS int
+LANGUAGE plpgsql
+AS $$
+DECLARE
+
+-- Declare Variables
+
+return_msg int;
+
+BEGIN
+
+-- Insert into records table
+
+INSERT INTO records.records (User_Id, GP_Id, Record_Details, Practice)
+-- Using Subqueries to find the relevant foreign keys to insert into the table from Email and Names
+VALUES ((SELECT User_Id FROM patients.patients WHERE Email=EPatientEmail),
+(SELECT Gp_Id FROM gp.gp WHERE First_Name=EGPFirstName AND Last_Name=EGPLastName),
+ERecordDetails, EPractice);
+RETURNING user_id INTO return_msg;
+
+-- Return a message
+
+RETURN return_msg;
+
+END;
+$$;
---
+++ Pro300/sqlcode/record_add_master.sql
@@ -0,0 +1,87 @@
+-- This Procedure will work with Flask API to add a record to the patient Database
+-- It will search to ensure the patient exists, ensuring data
+
+CREATE OR REPLACE PROCEDURE add_record_master(EPatientEmail VARCHAR(50),
+EGPLastName VARCHAR(50),
+ERecordDetails TEXT,
+EPractice VARCHAR(50))
+LANGUAGE plpgsql
+AS $$
+DECLARE
+-- Variables go here
+
+ValidEmailChk SMALLINT;
+ValidGPChk SMALLINT;
+ValidGenderChk SMALLINT;
+NewRecordId INT;
+
+BEGIN
+-- 1. Read in Data 
+
+-- Query the Patient Table to see if the email already exists
+
+SELECT COUNT(email)
+INTO ValidEmailChk
+FROM patients.patients
+WHERE email = EEmail;
+
+-- Query the GP Table to see if the GP already exists (Retreived through enviornmental variable)
+
+SELECT COUNT(firstname)
+INTO ValidGPChk
+FROM gp.gp
+WHERE firstname = EGPFirstName AND lastname = EGPLastName;
+
+-- Query the Practuce Table to see if the Practice exists
+
+--SELECT COUNT(email)
+--INTO ValidEmailChk
+--FROM patients.patients
+--WHERE email = EEmail;
+
+-- 2. Perform Logcial Operations on the Data
+
+-- Check if email is in a valid format as per constraint
+IF (EPatientEmail NOT LIKE ('%@%_._%')) THEN
+	RAISE EXCEPTION 'Invalid Email format detected: %', EPatientEmail
+		USING hint = 'Validate Email';
+END IF;
+
+-- Check if Patient Exists
+IF (ValidEmailChk = 0) THEN
+	RAISE EXCEPTION 'Patient does not exist: '
+		USING hint = 'Please check details entered';
+END IF;
+
+-- Check if GP Exists
+IF (ValidGPChk = 0) THEN
+        RAISE EXCEPTION 'Patient does not exist: '
+                USING hint = 'Please check details entered';
+END IF;
+
+--IF (EEmail NOT LIKE ('%@%_._%')) THEN
+--        RAISE EXCEPTION 'Invalid Email format detected: %', EEmail
+--                USING hint = 'Validate Email';
+--END IF;
+
+-- 3. Perform Data Insertion with Error Handling
+
+-- Try
+BEGIN
+	-- Insert Into Record Table
+	NewRecordId := add_record_details(EPatientEmail,
+EGPFirstName,
+EGPLastName,
+ERecordDetails,
+EPractice);
+	RAISE NOTICE 'User Created: %', NewRecordId;
+
+-- Handling Errors Gracefully
+EXCEPTION
+	-- Fallback
+	WHEN others THEN
+	PERFORM error_log(SQLERRM);
+	RAISE NOTICE 'Error Logged. Contact Admin';
+END;
+	
+end; $$;
