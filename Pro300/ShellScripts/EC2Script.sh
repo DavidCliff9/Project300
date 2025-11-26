e05497ab21b659d4fc6cf13f78f28c56277e7e2c
DavidCliff9 <157513406+DavidCliff9@users.noreply.github.com>
2025-11-26 15:01:10 +0000

    Update EC2Script.sh

--- Pro300/ShellScripts/EC2Script.sh
+++ Pro300/ShellScripts/EC2Script.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-wget https://raw.githubusercontent.com/Medical-Record-System/Postgres-Docker/main/AWSDockerInstall.sh
+wget https://raw.githubusercontent.com/DavidCliff9/PostgresDockerIntegration/refs/heads/main/AWSDockerInstall.sh
 sudo chmod 777 AWSDockerScript.sh
 ./AWSDockerScript.sh
 docker-compose up
