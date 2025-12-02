955a011c1b3fb74c6e6cc9f957e68efc391cc9c3
DavidCliff9 <157513406+DavidCliff9@users.noreply.github.com>
2025-12-02 11:38:07 +0000

    Update EC2Script.sh

--- Pro300/ShellScripts/EC2Script.sh
+++ Pro300/ShellScripts/EC2Script.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-wget https://raw.githubusercontent.com/DavidCliff9/PostgresDockerIntegration/refs/heads/main/AWSDockerInstall.sh
+wget https://raw.githubusercontent.com/DavidCliff9/PostgresDockerIntegration/refs/heads/main/AWSDockerScript.sh
 sudo chmod 777 AWSDockerScript.sh
 ./AWSDockerScript.sh
 docker-compose up
